68e361612b0e006a90b451bf3cd939f88ca291fa
Дмитрий Фроленко <orangefrol@gmail.com>
2022-12-31 02:35:35 +0300

    fixed a github link

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ long_description=(this_directory / 'README.MD').read_text(encoding='utf8')
 
 setup(
     name='nf_presentation',
-    version='0.1.1',
+    version='0.1.2',
     author='Dmitriy Frolenko',
     author_email='orangefrol@gmail.com',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     install_requires=requirements, # лол без этой строчки он ругался на README.MD
-    url='https://github.com/thefrol/nanofootball-presentation',
+    url='https://github.com/thefrol/nanofootball-presentation-maker',
     long_description=long_description,
     long_description_content_type='text/markdown; variant=GFM',
     license="MIT",
